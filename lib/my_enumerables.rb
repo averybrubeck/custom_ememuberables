53e8956b2751135b65c3cd7e4b5886d502566fac
averybrubeck <averybrubeck@yahoo.com>
2024-01-17 21:29:39 -0500

    added each with index

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,41 @@
 module Enumerable
-  # Your code goes here
+
+  def my_all?
+    my_each { |elem| return false unless yield(elem) }
+    true
+  end
+
+  def my_any
+    my_each { |elem| }
+  end
+  
+  def my_count
+
+  end
+
+  def my_each_with_index
+    i = 0
+    for elem in self
+      yield(elem, i)
+      i += 1
+    end
+  end
+
+  def my_inject
+
+  end
+
+  def my_map
+
+  end
+
+  def my_none
+
+  end
+
+  def my_select
+
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +43,10 @@ end
 # your enumerable module will have access
 # to this method
 class Array
- include Enumerable
+
+  def my_each
+    for elem in self
+      yield(elem)
+    end
+  end
 end
